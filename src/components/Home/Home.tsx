c3c0b05a495290171821000a2052d232bec1ac01
Aruba92 <aborrell92@gmail.com>
2024-03-20 11:11:40 +0100

    toggleOrder_DONE

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,48 +1,51 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import GetUser from "../../services/APIcall";
 import "./home.css";
 
 export default function Home () {
 
-    const [data, setData] = useState<any[]>([]);
-    const [colorButton, setColorButton] = useState(false);
+    const hasLoadedBefore = useRef(true); //for avoid two initial apiCalls every time (react do 2 renders)
+    //let apiData:any[] = [];
+    const [apiData, setApiData] = useState<any[]>([]);
+    const [tableData, setTableData] = useState<any[]>([]);
+    const [toggleColor, setToggleColor] = useState(false);
+    const [toggleOrder, setToggleOrder] = useState(false);
 
-    let results:any[] = [];
 
-    useEffect(()=>{
-        GetUser(setData);
-    },[]);
-
-    getRowsData();
-    function getRowsData () {
-        if (data.length > 0){
-            results = data.map((element:any, k:number)=>{
-                return(
-                    <tr>
-                        <td key={k}>
-                            <img src={element.picture.thumbnail}></img>
-                        </td>
-                        <td key={k}>{element.name.first}</td>
-                        <td key={k}>{element.name.last}</td>
-                        <td key={k}>{element.location.country}</td>
-                        <td key={k}>BORRAR</td>
-                    </tr>
-                )
-            })
+    useEffect(()=>{ //set apiData and tableDate with the return from the apiCall
+        if (hasLoadedBefore.current){
+            GetUser().then((result)=>{
+                setApiData(result);
+                setTableData(result);
+            });
+            hasLoadedBefore.current = false;
         }
-    }
+    },[apiData]);
 
-    function toggleColor () {
-        setColorButton(!colorButton);
-    }
+    useEffect(()=>{ //Change the Data in the table according to toggleOrder to Order or Disorder the data.
+        if (toggleOrder) {
+            let newArray = [...apiData];
+            let dataSorted = newArray.sort((a, b) => a.location.country.localeCompare(b.location.country));
+            setTableData([
+                ...dataSorted //spread syntax
+            ]);
+        }else{
+            if (apiData.length > 0) {
+                setTableData([
+                    ...apiData
+                ]);
+            }
+            
+        }
+    },[toggleOrder])
+
     
-
     return(
         <main>
             <h1>Prueba Técnica</h1>
             <div className="buttons">
-                <button className="addColorButton" onClick={toggleColor}>Colorear</button>
-                <button className="orderByCountry">Ordenar por país</button>
+                <button className="addColorButton" onClick={()=>setToggleColor(!toggleColor)}>Colorear</button>
+                <button className="orderByCountry" onClick={()=>setToggleOrder(!toggleOrder)}>Ordenar por país</button>
                 <button className="restartState">Resetear estado</button>
                 <input className="filterByCountry"></input>
             </div>
@@ -56,8 +59,22 @@ export default function Home () {
                         <th>Acciones</th>
                     </tr>
                 </thead>
-                <tbody className={colorButton ? "colorActive" : ""}>
-                    {results}
+                <tbody className={toggleColor ? "colorActive" : ""}>
+                    {tableData.map((element:any, k:number)=>{
+                        return(
+                            <tr key={k+"0"}>
+                                <td key={k+"1"}>
+                                    <img src={element.picture.thumbnail}></img>
+                                </td>
+                                <td key={k+"2"}>{element.name.first}</td>
+                                <td key={k+"3"}>{element.name.last}</td>
+                                <td key={k+"4"}>{element.location.country}</td>
+                                <td key={k+"5"}>
+                                    <button>Borrar</button>
+                                </td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
         </main>
--- src/services/APIcall.tsx
+++ src/services/APIcall.tsx
@@ -1,8 +1,15 @@
 
 
-export default async function GetUser (setData:any) {
+/* export default async function GetUser (setData:any) {
     const response = await fetch("https://randomuser.me/api/?results=100");
     const data = await response.json();
     console.log(data.results);
     setData(data.results);
+} */
+
+export default async function GetUser (): Promise<any[]>{
+    const response = await fetch("https://randomuser.me/api/?results=100");
+    const data = await response.json();
+    console.log(data.results);
+    return data.results;
 }
